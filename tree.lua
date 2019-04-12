97ecef287e1ef6d494fa0310f0afb923c92f4f67
san590 <san590@interia.pl>
2019-04-12 23:56:29 +0200

    new feature: tree has trunk

--- tree.lua
+++ tree.lua
@@ -1,48 +1,64 @@
 while(1) do
-    io.write( "Podaj wysokosc drzewa: ") 
-    height = io.read()
-  
-    if(tonumber(height) < 3) then
-      os.execute("clear")
-    else
-      break end
+  io.write( "Podaj wysokosc drzewa: ") 
+  height = io.read()
+
+  if(tonumber(height) < 3) then
+    os.execute("clear")
+  else
+    break end
+end
+
+sign = '*'
+width = (height - 1) * 2 + 1
+halfWidth = math.floor(width/2) + 1
+print(halfWidth)
+
+function printSpace() 
+  io.write(" ")
+end
+
+function printSign()
+  io.write(sign)
+end
+
+function newLine()
+  print()
+end
+
+function drawTrunk()
+  for i=height+1, height+math.floor(width/2) do
+      for j=1, width do
+        if(j == halfWidth + 1) then
+          printSign()
+        else printSpace() end
+
+        if(j == width) then
+          newLine()
+        end
+      end
   end
+end
+
+for i=1, height do
   
-  sign = '*'
-  width = (height - 1) * 2 + 1
-  halfWidth = math.floor(width/2) + 1
-  print(halfWidth)
-  
-  function printSpace() 
-    io.write(" ")
+  for j=1, (halfWidth-i+1) do
+    printSpace()
   end
-  
-  function printSign()
-    io.write(sign)
+
+  for k=(halfWidth-i+1), (halfWidth+i-1) do
+    printSign()
+
+    if(k == (halfWidth+i-1)) then
+      newLine()
+    end
+
   end
-  
-  function newLine()
-    print()
-  end
-  
-  for i=1, height do
-    
-    for j=1, (halfWidth-i+1) do
-      printSpace()
+
+
+ if(i == width) then
+      newLine()
     end
   
-    for k=(halfWidth-i+1), (halfWidth+i-1) do
-      printSign()
-  
-      if(k == (halfWidth+i-1)) then
-        newLine()
-      end
-  
-    end
-  
-  
-   if(i == width) then
-        newLine()
-      end
-    
-  end  
+end
+
+drawTrunk()  
