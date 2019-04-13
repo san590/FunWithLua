6b91ca74fdec18c6da25c4723b21e0439fbd31e2
san590 <46568905+san590@users.noreply.github.com>
2019-04-13 23:32:19 +0200

    Logic is done

--- codeCracker.lua
+++ codeCracker.lua
@@ -2,6 +2,10 @@ math.randomseed(os.time())
 local Number = {}
 Number.__index = Number
 
+function space()
+  io.write(" ")
+end
+
 function newLine()
   print()
 end
@@ -13,6 +17,7 @@ end
 function Number.new()
   local self = setmetatable({}, Number)
   self.value = getRandomValue()
+  self.shownValue = "_"
   return self
 end
 
@@ -24,28 +29,90 @@ function Number.printValue(self)
   io.write(self.value)
 end
 
-function Number.isEqual(self, value)
-  if (self.value == tonumber(value)) then
-    io.write("Liczby są takie same.")
-    newLine()
-    Number.printValue(self)
-  else
-    io.write("Liczby nie są takie same.")
-    newLine()
-    Number.printValue(self)
+function Number.replace(self)
+  self.shownValue = self.shownValue:gsub("_", tostring(self.value))
+end
+
+function Number.isEqual(self, givenValue)
+  isEqual = 0
+
+  if (self.value == givenValue) then
+    Number.replace(self)
+    isEqual = 1
+  end
+
+  Number.show(self)
+  
+  return isEqual
+end
+
+function Number.show(self)
+  io.write(self.shownValue)
+  for i=1, 2 do
+    space()
   end
 end
 
 function getInput()
   io.write("Podaj liczbę: ")
   liczba = io.read()
-  return liczba
+  os.execute("clear")
+  print("CodeCracker")
+  io.write("Podano liczbę: " .. liczba)
+  newLine()
+  return tonumber(liczba)
 end
 
-function sprawdzamCzyDziala()
-  local i = Number.new()
-  liczba = getInput()
-  i:isEqual(liczba)
+function showLives(lives)
+  try = '+'
+  tried = 'x'
+  
+  newLine()
+  io.write("Lives: ")
+
+  for i=1, lives do
+    io.write(try)
+  end
+
+  for i=1, 3-lives do
+    io.write(tried)
+  end
+
 end
 
-sprawdzamCzyDziala()
+
+function game()
+  local firstNumber = Number.new()
+  local secondNumber = Number.new()
+  local thirdNumber = Number.new()
+  local fourthNumber = Number.new()
+
+  lives = 3
+
+  while true do
+    print("CodeCracker")
+    liczba = getInput()
+    isEqual = 0
+
+    isEqual = isEqual + firstNumber:isEqual(liczba)
+    isEqual = isEqual + secondNumber:isEqual(liczba)
+    isEqual = isEqual + thirdNumber:isEqual(liczba)
+    isEqual = isEqual + fourthNumber:isEqual(liczba)
+
+    if(isEqual == 0) then
+      lives = lives - 1
+    end
+
+    showLives(lives)
+
+    if(lives == 0) then
+      break
+    end
+
+   
+    
+  end
+
+end
+
+game()
